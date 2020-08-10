29f6f0f04ca375d9316931c1bd98bcf9805a338b
loudam88 <loudam88@gmail.com>
2020-08-10 11:30:50 -0500

    Fixed api back to local

--- curl-scripts/albums/index.sh
+++ curl-scripts/albums/index.sh
@@ -2,7 +2,7 @@
 # deployed site: https://git.heroku.com/intense-scrubland-71825.git
 # http://localhost:4741
 
-API="https://git.heroku.com/intense-scrubland-71825.git"
+API="http://localhost:4741"
 URL_PATH="/albums"
 
 curl "${API}${URL_PATH}" \
